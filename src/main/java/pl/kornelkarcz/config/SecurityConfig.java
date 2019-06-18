426e08ba94812cf9361c4415f5fc9a0ebd5c48bb
kornelkarcz <kornel.karcz@outlook.com>
2019-06-18 23:32:18 +0200

    Update security config

--- src/main/java/pl/kornelkarcz/config/SecurityConfig.java
+++ src/main/java/pl/kornelkarcz/config/SecurityConfig.java
@@ -1,15 +1,26 @@
 package pl.kornelkarcz.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
+
+    
     protected void configure(HttpSecurity http) throws Exception {
 
         http.
