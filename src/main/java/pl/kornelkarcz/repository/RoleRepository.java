a4cb7f79ce45fdcb26af1b404b26b367ddb35c2d
kornelkarcz <kornel.karcz@outlook.com>
2019-06-18 19:23:27 +0200

    Create Role repo

---
+++ src/main/java/pl/kornelkarcz/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package pl.kornelkarcz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.kornelkarcz.model.Role;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRole(String role);
+}
