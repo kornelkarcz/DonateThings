bac4f4f8d8566d81bfc97efe8cd81446d5a75984
kornelkarcz <kornel.karcz@outlook.com>
2019-06-18 19:50:39 +0200

    Create user service

---
+++ src/main/java/pl/kornelkarcz/service/UserService.java
@@ -0,0 +1,39 @@
+package pl.kornelkarcz.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.kornelkarcz.model.Role;
+import pl.kornelkarcz.model.User;
+import pl.kornelkarcz.repository.RoleRepository;
+import pl.kornelkarcz.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service("userService")
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
+}
