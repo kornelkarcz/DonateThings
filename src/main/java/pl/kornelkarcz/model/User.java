f51a3852f36cccc3afb8ac9eeca6afccafef7593
kornelkarcz <kornel.karcz@outlook.com>
2019-06-18 19:10:28 +0200

    Create Role model class

--- src/main/java/pl/kornelkarcz/model/User.java
+++ src/main/java/pl/kornelkarcz/model/User.java
@@ -6,6 +6,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import java.util.Set;
 
 @Data
 @Entity
