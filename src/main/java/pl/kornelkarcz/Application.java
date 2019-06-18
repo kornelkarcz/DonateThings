78713c457514a5b703da900f3951ac2ec7f0c923
kornelkarcz <kornel.karcz@outlook.com>
2019-06-18 19:22:04 +0200

    Create user repo

--- src/main/java/pl/kornelkarcz/Application.java
+++ src/main/java/pl/kornelkarcz/Application.java
@@ -1,4 +1,4 @@
-package pl.kornelkarcz.app;
+package pl.kornelkarcz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/pl/kornelkarcz/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package pl.kornelkarcz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.kornelkarcz.model.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
+}
