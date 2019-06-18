5388dd00c9452941fb623fe89fd8e6bcc671f4fe
kornelkarcz <kornel.karcz@outlook.com>
2019-06-18 19:57:09 +0200

    Create login html

--- src/main/java/pl/kornelkarcz/controller/LoginController.java
+++ src/main/java/pl/kornelkarcz/controller/LoginController.java
@@ -1,7 +1,17 @@
 package pl.kornelkarcz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public ModelAndView showLogin(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        modelAndView.addObject("loginMessage", "Please login");
+        return modelAndView;
+    }
 }
