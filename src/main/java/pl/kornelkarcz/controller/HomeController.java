ab65d3fc72ce81447a68897ad5d95712496272ec
kornelkarcz <kornel.karcz@outlook.com>
2019-06-23 15:40:56 +0200

    Update login controller

--- src/main/java/pl/kornelkarcz/controller/HomeController.java
+++ src/main/java/pl/kornelkarcz/controller/HomeController.java
@@ -2,24 +2,17 @@ package pl.kornelkarcz.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 import pl.kornelkarcz.config.CurrentUser;
-import pl.kornelkarcz.model.User;
 import pl.kornelkarcz.service.UserService;
 
-import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
-@SessionAttributes({"loggedUser"})
 public class HomeController {
 
     private final UserService userService;
@@ -29,47 +22,6 @@ public class HomeController {
         String message = "Hello Anonymous User!";
         model.addAttribute("helloMessage", message);
         return "index";
-    }
-
-    @GetMapping("/login")
-    public String getLoginPage() {
-        return "login";
-    }
-
-    @GetMapping("/register")
-    public String getRegisterPage(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "register";
-    }
-
-    @PostMapping("/register")
-    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
-        ModelAndView modelAndView = new ModelAndView();
-        User userExists = userService.findUserByEmail(user.getEmail());
-        if (userExists != null) {
-            bindingResult
-                    .rejectValue("email", "error.user", "There is already a user registered with the email provided");
-        }
-        if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("register");
-        } else {
-            userService.saveUser(user);
-            modelAndView.addObject("successMessage", "User has been registered successfully.");
-            modelAndView.addObject("user", new User());
-            modelAndView.setViewName("index");
-        }
-        return modelAndView;
-    }
-
-    @GetMapping("/loginSuccess")
-    public String loginSuccess(Model model, @AuthenticationPrincipal CurrentUser currentUser) {
-        User loggedUser = currentUser.getUser();
-        model.addAttribute("loggedUser", loggedUser);
-        Long id = loggedUser.getId();
-        String successLogin = "Hello, " + id + ", you have successfully logged in";
-        model.addAttribute("logSuccessMessage", successLogin);
-        return "loginSuccess";
     }
 
     @GetMapping("/userDetails")
--- src/main/java/pl/kornelkarcz/controller/LoginController.java
+++ src/main/java/pl/kornelkarcz/controller/LoginController.java
@@ -1,79 +1,67 @@
-//package pl.kornelkarcz.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.SessionAttributes;
-//import org.springframework.web.servlet.ModelAndView;
-//import pl.kornelkarcz.config.CurrentUser;
-//import pl.kornelkarcz.model.User;
-//import pl.kornelkarcz.service.UserService;
-//
-//import javax.validation.Valid;
-//
-//@Controller
-//@SessionAttributes({"currentUser"})
-//public class LoginController {
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @GetMapping("/login")
-//    public ModelAndView login(ModelMap model, @AuthenticationPrincipal CurrentUser currentUser) {
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("login");
-//        modelAndView.addObject("loginMessage", "Please login");
-//
-//        if (currentUser == null) {
-//            model.addAttribute("user", new User());
-//            ModelAndView modelAndView1 = new ModelAndView();
-//            modelAndView1.setViewName("login");
-//            return modelAndView1;
-//        } else
-//
-//
-////        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-////        String name = auth.getName(); //get logged in username
-////
-////        model.addAttribute("username", name);
-//
-//
-//        return modelAndView;
-//    }
-//
-//
-//    @GetMapping("/register")
-//    public ModelAndView showRegisterPage() {
-//        ModelAndView modelAndView = new ModelAndView();
-//        User user = new User();
-//        modelAndView.addObject("user", user);
-//        modelAndView.setViewName("register");
-//        return modelAndView;
-//    }
-//
-//    @PostMapping("/register")
-//    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
-//        ModelAndView modelAndView = new ModelAndView();
-//        User userExists = userService.findUserByEmail(user.getEmail());
-//        if (userExists != null) {
-//            bindingResult
-//                    .rejectValue("email", "error.user", "There is already a user registered with the email provided");
-//        }
-//        if (bindingResult.hasErrors()) {
-//            modelAndView.setViewName("register");
-//        } else {
-//            userService.saveUser(user);
-//            modelAndView.addObject("successMessage", "User has been registered successfully.");
-//            modelAndView.addObject("user", new User());
-//            modelAndView.setViewName("register");
-//        }
-//        return modelAndView;
-//    }
-//}
+package pl.kornelkarcz.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+import pl.kornelkarcz.config.CurrentUser;
+import pl.kornelkarcz.model.User;
+import pl.kornelkarcz.service.UserService;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequiredArgsConstructor
+@SessionAttributes({"loggedUser"})
+public class LoginController {
+
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(Model model, @AuthenticationPrincipal CurrentUser currentUser) {
+        User loggedUser = currentUser.getUser();
+        model.addAttribute("loggedUser", loggedUser);
+        Long id = loggedUser.getId();
+        String successLogin = "Hello, " + id + ", you have successfully logged in";
+        model.addAttribute("logSuccessMessage", successLogin);
+        return "loginSuccess";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user", "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("register");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully.");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("index");
+        }
+        return modelAndView;
+    }
+
+}
