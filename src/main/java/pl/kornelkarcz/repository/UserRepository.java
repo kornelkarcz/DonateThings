420b29dde85862476fade89582bebea805f404a9
kornelkarcz <kornel.karcz@outlook.com>
2019-07-02 21:30:56 +0200

    Update user repo

--- src/main/java/pl/kornelkarcz/repository/UserRepository.java
+++ src/main/java/pl/kornelkarcz/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kornelkarcz.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findUserByEmail(String email);
 
 }
