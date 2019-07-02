7261a5a2b7b48920770ad9ef26dcef3cd062fb88
kornelkarcz <kornel.karcz@outlook.com>
2019-07-02 21:22:59 +0200

    Create verification token class

--- src/main/java/pl/kornelkarcz/model/VerificationToken.java
+++ src/main/java/pl/kornelkarcz/model/VerificationToken.java
@@ -1,24 +1,18 @@
 package pl.kornelkarcz.model;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Date;
 
 @Entity
-@Getter
-@Setter
-@RequiredArgsConstructor
+@Table
 public class VerificationToken {
 
     private static final int EXPIRATION = 60 * 24;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String token;
@@ -27,7 +21,70 @@ public class VerificationToken {
     @JoinColumn(nullable = false, name = "user_id")
     private User user;
 
+    private Date createdDate;
     private Date expiryDate;
+
+
+    public VerificationToken() {
+        super();
+    }
+
+    public VerificationToken(final String token) {
+        super();
+
+        this.token = token;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    public VerificationToken(final String token, final User user) {
+        super();
+        Calendar calendar = Calendar.getInstance();
+
+        this.token = token;
+        this.user = user;
+        this.createdDate = new Date(calendar.getTime().getTime());
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 
     private Date calculateExpiryDate(int expiryTimeInMinutes) {
         Calendar calendar = Calendar.getInstance();
