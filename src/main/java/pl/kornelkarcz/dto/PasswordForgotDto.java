d8dd867e29f9070b8ac6b1abfdf4e7d71629bd87
kornelkarcz <kornel.karcz@outlook.com>
2019-07-08 14:06:10 +0200

    Create forgot password dto

--- src/main/java/pl/kornelkarcz/dto/PasswordForgotDto.java
+++ src/main/java/pl/kornelkarcz/dto/PasswordForgotDto.java
@@ -1,14 +1,18 @@
 package pl.kornelkarcz.dto;
 
 import lombok.Data;
-import pl.kornelkarcz.validator.ValidEmail;
+import pl.kornelkarcz.validator.PasswordMatches;
+import pl.kornelkarcz.validator.ValidPassword;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@PasswordMatches
 public class PasswordForgotDto {
 
-    @ValidEmail
+    @ValidPassword
     @NotEmpty
-    private String email;
+    private String newPassword;
+    private String matchingPassword;
+
 }
