015360580b83f96e6bbc18688acd719ad6341638
kornelkarcz <kornel.karcz@outlook.com>
2019-07-09 20:18:41 +0200

    Add validators

---
+++ src/main/java/pl/kornelkarcz/validator/FieldMatch.java
@@ -0,0 +1,31 @@
+package pl.kornelkarcz.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = FieldMatchValidator.class)
+public @interface FieldMatch {
+
+    String message() default "";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String first();
+    String second();
+
+    @Target({TYPE, ANNOTATION_TYPE})
+    @Retention(RUNTIME)
+    @Documented
+    @interface List
+    {
+        FieldMatch[] value();
+    }
+
+
+}
