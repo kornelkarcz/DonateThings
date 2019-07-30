14a4ce5f5ea13579d602dbe666b98495e17d48cf
kornelkarcz <kornel.karcz@outlook.com>
2019-07-30 14:02:48 +0200

    Add deleting old tokens method

--- src/main/java/pl/kornelkarcz/repository/VerificationTokenRepository.java
+++ src/main/java/pl/kornelkarcz/repository/VerificationTokenRepository.java
@@ -1,6 +1,8 @@
 package pl.kornelkarcz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import pl.kornelkarcz.model.User;
 import pl.kornelkarcz.model.VerificationToken;
 
@@ -9,4 +11,9 @@ public interface VerificationTokenRepository extends JpaRepository<VerificationT
     VerificationToken findByToken(String token);
 
     VerificationToken findByUser(User user);
+
+    @Modifying
+    @Query(value = "delete from verification_token where user_id = ?1",
+    nativeQuery = true)
+    void deleteVerificationTokenByUserId(Long id);
 }
